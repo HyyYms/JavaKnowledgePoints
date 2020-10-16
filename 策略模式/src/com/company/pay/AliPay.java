1efc898b7ea29619970274908509d4eae6b561a2
YaYaHuang <570911275@qq.com>
2020-10-16 10:09:04 +0800

    策略模式的支付案示例

---
+++ 策略模式/src/com/company/pay/AliPay.java
@@ -0,0 +1,19 @@
+package com.company.pay;
+
+/**
+ * @program: 策略模式
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-10-16 09:52
+ **/
+public class AliPay extends Payment{
+    @Override
+    public String getName() {
+        return "支付宝";
+    }
+
+    @Override
+    protected double queryBalance(String uid) {
+        return 520;
+    }
+}
---
+++ 策略模式/src/com/company/pay/JdPay.java
@@ -0,0 +1,19 @@
+package com.company.pay;
+
+/**
+ * @program: 策略模式
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-10-16 09:52
+ **/
+public class JdPay extends Payment{
+    @Override
+    public String getName() {
+        return "京东支付";
+    }
+
+    @Override
+    protected double queryBalance(String uid) {
+        return 123;
+    }
+}
