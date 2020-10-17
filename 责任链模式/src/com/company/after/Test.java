56bacfec027b8ab80fca0d53de953d88cbdc5dff
YaYaHuang <570911275@qq.com>
2020-10-17 23:25:35 +0800

    迭代器模式的架构

---
+++ 责任链模式/src/com/company/after/Test.java
@@ -0,0 +1,8 @@
+package com.company.after;
+
+public class Test {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberService();
+        memberService.login("huang", "123");
+    }
+}
