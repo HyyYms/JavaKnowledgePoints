feb1cb63d4271d7400c08210ac669bb021cc2f1c
YaYaHuang <570911275@qq.com>
2020-10-27 11:35:31 +0800

    有效的括号的解法

---
+++ 算法与数据结构/栈/有效的括号/src/com/company/Solution.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @program: 有效的括号
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-10-27 11:07
+ **/
+public class Solution {
+
+    public static void main(String[] args) {
+        myIsValid("([)]");
+    }
+
+    public static boolean myIsValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        LinkedList<Character> stack = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (stack.size() == 0 || c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+                list.add(c);
+                System.out.println("栈："+stack);
+                System.out.println("链表："+list);
+                continue;
+            }
+            if (c == ')') {
+                if ('(' == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                    list.add(c);
+                    System.out.println("栈："+stack);
+                    System.out.println("链表："+list);
+                }
+                continue;
+            }
+            if (c == ']') {
+                if ('[' == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                    list.add(c);
+                    System.out.println("栈："+stack);
+                    System.out.println("链表："+list);
+                }
+                continue;
+            }
+            if (c == '}') {
+                if ('{' == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                    list.add(c);
+                    System.out.println("栈："+stack);
+                    System.out.println("链表："+list);
+                }
+            }
+        }
+        return stack.size() <= 0;
+    }
+}
