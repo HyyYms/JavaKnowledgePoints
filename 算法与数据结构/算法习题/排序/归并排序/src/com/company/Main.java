51d81c4e1b20e3e1676498212db50f394cb53c72
YaYaHuang <570911275@qq.com>
2020-11-05 22:50:11 +0800

    归并排序的代码示例

--- 算法与数据结构/算法习题/排序/归并排序/src/com/company/Main.java
+++ 算法与数据结构/算法习题/排序/归并排序/src/com/company/Main.java
@@ -3,6 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    private static void mergeSort(int[] arr) {
+        int[] temp = new int[arr.length];
+        mergeSortImpl(arr, 0, arr.length - 1, temp);
+    }
+
+    private static void mergeSortImpl(int[] arr, int start, int end, int[] temp) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSortImpl(arr, start, mid, temp);
+        mergeSortImpl(arr, mid + 1, end, temp);
+        merge(arr, start, mid, end, temp);
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end, int[] temp) {
+        int left = start;
+        int right = mid + 1;
+        int index = start;
+        while (left <= mid && right <= end) {
+            if (arr[left] < arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+        while (right <= end) {
+            temp[index++] = arr[right++];
+        }
+        for (index = start; index <= end; index++) {
+            arr[index] = temp[index];
+        }
     }
 }
