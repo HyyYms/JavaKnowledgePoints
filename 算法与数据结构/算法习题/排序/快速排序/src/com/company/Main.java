9258e665e3f0f98ef81a3a687bdfa14789a57b98
YaYaHuang <570911275@qq.com>
2020-11-05 22:15:44 +0800

    快速排序的代码示例

--- 算法与数据结构/算法习题/排序/快速排序/src/com/company/Main.java
+++ 算法与数据结构/算法习题/排序/快速排序/src/com/company/Main.java
@@ -3,6 +3,40 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = new int[]{4, 9, 6, 2, 1, 3};
+        quickSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    private static void quickSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int pivot = arr[start];
+        int left = start;
+        int right = end;
+        while (left <= right) {
+            while (left <= right && arr[left] < pivot) {
+                left++;
+            }
+            while (left <= right && arr[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        sort(arr, start, right);
+        sort(arr, left, end);
     }
 }
