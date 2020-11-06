4b85bcf5215a7b27129cbc592d8e4400ef7d2145
YaYaHuang <570911275@qq.com>
2020-11-06 10:54:08 +0800

    数据流中的中位数

--- 算法与数据结构/算法习题/堆/第K大元素/src/com/company/Solution.java
+++ 算法与数据结构/算法习题/堆/第K大元素/src/com/company/Solution.java
@@ -12,12 +12,51 @@ import java.util.PriorityQueue;
 public class Solution {
 
     public static void main(String[] args) {
-        new Solution().findKthLargest(new int[]{
+        new Solution().findKthLargestQuickSort(new int[]{
                 3, 2, 1, 5, 6, 4
         }, 3);
     }
 
     public int findKthLargestQuickSort(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 1 || k > nums.length) {
+            return -1;
+        }
+        return partition(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int partition(int[] arr, int start, int end, int k) {
+        if (start >= end) {
+            return arr[k];
+        }
+        int left = start;
+        int right = end;
+        int mid = start + (end - start) / 2;
+        int pivot = arr[mid];
+        while (left <= right) {
+            while (left <= right && arr[left] < pivot) {
+                left++;
+            }
+            while (left <= right && arr[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        if (k <= right) {
+            partition(arr, start, right, k);
+        }
+        if (k >= left) {
+            partition(arr, left, end, k);
+        }
+        return arr[k];
+    }
+
+    public int myFindKthLargestQuickSort(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k < 1 || k > nums.length) {
             return -1;
         }
