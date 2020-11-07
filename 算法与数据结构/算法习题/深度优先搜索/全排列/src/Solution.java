68218ddd36f6b3245bbb4ac164af74ac873f4ede
YaYaHuang <570911275@qq.com>
2020-11-07 22:37:54 +0800

    全排列的解法以及题目需求

--- 算法与数据结构/算法习题/深度优先搜索/全排列/src/Solution.java
+++ 算法与数据结构/算法习题/深度优先搜索/全排列/src/Solution.java
@@ -1,3 +1,7 @@
+import java.util.List;
+
+import java.util.ArrayList;
+
 /**
  * @program: 全排列
  * @description:
@@ -5,4 +9,37 @@
  * @create: 2020-11-07 22:16
  **/
 public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().permute(new int[]{1, 2, 3});
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null) {
+            return null;
+        }
+        if (nums.length == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        help(result, nums, list);
+        return result;
+    }
+
+    private void help(List<List<Integer>> result, int[] nums, ArrayList<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(nums[i]);
+            help(result, nums, list);
+            list.remove(list.size() - 1);
+        }
+    }
 }
