cf5e8d2928145cf27a4ef14442cc218967f7b873
YaYaHuang <570911275@qq.com>
2020-11-09 17:09:56 +0800

    小岛问题二的解法

--- 算法与数据结构/算法习题/并查集/小岛问题二/src/com/company/NumberOfIsland2.java
+++ 算法与数据结构/算法习题/并查集/小岛问题二/src/com/company/NumberOfIsland2.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.util.List;
@@ -12,10 +13,29 @@ import java.util.List;
  **/
 public class NumberOfIsland2 {
 
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        Point[] operators = new Point[4];
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(0, 1);
+        Point point3 = new Point(3, 3);
+        Point point4 = new Point(3, 4);
+        operators[0] = point1;
+        operators[1] = point2;
+        operators[2] = point3;
+        operators[3] = point4;
+        List<Integer> list = new NumberOfIsland2().numIslands(n, m, operators);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
+
     class UnionFind {
+
         private HashMap<Integer, Integer> father = new HashMap<Integer, Integer>();
 
-        public void unionFind(int row, int column) {
+        public UnionFind(int row, int column) {
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < column; j++) {
                     Integer id = convertId(i, j, row);
@@ -53,6 +73,39 @@ public class NumberOfIsland2 {
     }
 
     public List<Integer> numIslands(int n, int m, Point[] operators) {
+        List<Integer> result = new ArrayList<>();
+        if (operators == null || operators.length == 0) {
+            return result;
+        }
+        int count = 0;
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        UnionFind unionFind = new UnionFind(n, m);
+        boolean[][] isIsland = new boolean[n][m];
 
+        for (Point point : operators) {
+            int x = point.x;
+            int y = point.y;
+            if (!isIsland[x][y]) {
+                isIsland[x][y] = true;
+                count++;
+                Integer id = unionFind.convertId(x, y, n);
+                for (int k = 0; k < 4; k++) {
+                    int newX = x + dx[k];
+                    int newY = y + dy[k];
+                    if (newX >= 0 && newX < n && newY >= 0 && newY < m && isIsland[newX][newY]) {
+                        Integer newId = unionFind.convertId(newX, newY, n);
+                        Integer father = unionFind.find(id);
+                        Integer nfather = unionFind.find(newId);
+                        if (!father.equals(nfather)) {
+                            count--;
+                            unionFind.union(nfather, father);
+                        }
+                    }
+                }
+            }
+            result.add(count);
+        }
+        return result;
     }
 }
