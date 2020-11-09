20375f5d352f40a895083bf4e5c835e6e4a1dd5f
YaYaHuang <570911275@qq.com>
2020-11-09 16:25:40 +0800

    并查集的基本模板代码示例

---
+++ 算法与数据结构/算法习题/并查集/并查集/src/com/company/UnionFind.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.HashMap;
+
+/**
+ * @program: 并查集
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-11-09 16:12
+ **/
+public class UnionFind {
+
+
+    HashMap<Integer, Integer> father = new HashMap<>();
+
+    public void unionFind(Integer n) {
+        for (int i = 0; i < n; i++) {
+            father.put(i, i);
+        }
+    }
+
+    public Integer find(Integer x) {
+        Integer parent = father.get(x);
+        while (!parent.equals(father.get(parent))) {
+            parent = father.get(parent);
+        }
+        Integer temp = -1;
+        Integer fa = father.get(x);
+        while (!fa.equals(father.get(fa))) {
+            temp = father.get(fa);
+            father.put(fa, father.get(fa));
+            fa = temp;
+        }
+        return parent;
+    }
+
+    public void union(Integer x, Integer y) {
+        Integer fa_x = father.get(x);
+        Integer fa_y = father.get(y);
+        if (!fa_x.equals(fa_y)) {
+            father.put(fa_x, fa_y);
+        }
+    }
+}
