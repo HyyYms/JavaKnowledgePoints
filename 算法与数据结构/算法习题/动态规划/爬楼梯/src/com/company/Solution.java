b973a2894cefe754ffd83a7529b26c816499d3b6
YaYaHuang <570911275@qq.com>
2020-11-10 23:10:16 +0800

    爬楼梯的不同解法

--- 算法与数据结构/算法习题/动态规划/爬楼梯/src/com/company/Solution.java
+++ 算法与数据结构/算法习题/动态规划/爬楼梯/src/com/company/Solution.java
@@ -9,17 +9,34 @@ package com.company;
 public class Solution {
 
     public int climbStairs(int n) {
-        if (n <= 3) {
+        /*
+           时间过长
+            if (n <= 3) {
+                return n;
+            }
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        */
+        /*
+            我的解法
+            int num1 = 0;
+            int num2 = 0;
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                num1 = num2;
+                num2 = result;
+                result = num1 + num2;
+            }
+            return result;
+        */
+        if (n < 3) {
             return n;
         }
-        int num1 = 0;
-        int num2 = 0;
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            num1 = num2;
-            num2 = result;
-            result = num1 + num2;
+        int[] steps = new int[3];
+        steps[0] = 1;
+        steps[1] = 2;
+        for (int i = 2; i < n; i++) {
+            steps[i % 3] = steps[(i - 1) % 3] + steps[(i - 2) % 3];
         }
-        return result;
+        return steps[(n - 1) % 3];
     }
 }
