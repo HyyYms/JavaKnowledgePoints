65abb6a221d28f7b881dba5d9078618849eadf79
YaYaHuang <570911275@qq.com>
2020-11-15 15:48:54 +0800

    回文分割的解法

--- 算法与数据结构/算法习题/动态规划/回文分割/src/com/company/Main.java
+++ 算法与数据结构/算法习题/动态规划/回文分割/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s = "leetcode";
+        System.out.println(s.charAt(8));
+        // write your code here
     }
 }
