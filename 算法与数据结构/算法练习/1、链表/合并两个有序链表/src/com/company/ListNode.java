1dd2d2f36f2cdb2c295413e8137e9e954d311522
YaYaHuang <570911275@qq.com>
2020-11-17 23:00:25 +0800

    合并两个有序链表的解法

---
+++ 算法与数据结构/算法练习/1、链表/合并两个有序链表/src/com/company/ListNode.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * @program: 合并两个有序链表
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-11-16 23:12
+ **/
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
