21eae73863522d5183ddb987f3f62ea1506a996c
YaYaHuang <570911275@qq.com>
2020-12-14 23:48:24 +0800

    移除元素的解法以及题目需求

---
+++ 算法与数据结构/算法练习/5、双指针/移除元素/src/com/company/Solution.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Arrays;
+
+/**
+ * @program: 移除元素
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-12-14 23:32
+ **/
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
