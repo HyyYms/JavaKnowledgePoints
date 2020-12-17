dd8e6e7375f6511ac9061e83c67e76004e0fc389
YaYaHuang <570911275@qq.com>
2020-12-18 00:54:31 +0800

    最大连续1的个数的题目架构

---
+++ 算法与数据结构/算法练习/5、双指针/替换后的最长重复字符/src/com/company/Solution2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * @program: 最大连续1的个数
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-12-17 23:48
+ **/
+public class Solution2 {
+
+    public int longestOnes(int[] A, int K) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int
+    }
+}
