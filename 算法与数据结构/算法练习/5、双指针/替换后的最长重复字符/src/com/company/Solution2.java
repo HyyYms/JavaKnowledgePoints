aa5e06777f3fd80d2334b426e641dbc7f576ddff
YaYaHuang <570911275@qq.com>
2020-12-19 23:46:52 +0800

    最大连续1的个数的解法以及题目需求

--- 算法与数据结构/算法练习/5、双指针/替换后的最长重复字符/src/com/company/Solution2.java
+++ 算法与数据结构/算法练习/5、双指针/替换后的最长重复字符/src/com/company/Solution2.java
@@ -12,6 +12,22 @@ public class Solution2 {
         if (A == null || A.length == 0) {
             return 0;
         }
-        int
+        int left = 0;
+        int right = 0;
+        int num = 0;
+        int historyOnesMax = 0;
+        for (right = 0; right < A.length; right++) {
+            if (A[right] == 1) {
+                num++;
+            }
+            historyOnesMax = Math.max(num, historyOnesMax);
+            if (right - left + 1 > historyOnesMax + K) {
+                if (A[left] == 1) {
+                    num--;
+                }
+                left++;
+            }
+        }
+        return A.length - left;
     }
 }
