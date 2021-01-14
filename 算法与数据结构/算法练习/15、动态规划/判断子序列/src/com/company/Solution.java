3fdf1fa2f1c9945acb957c8b1ed9a27ed5394c1d
YaYaHuang <570911275@qq.com>
2021-01-14 14:50:13 +0800

    判断子序列的题目需求与解法

--- 算法与数据结构/算法练习/15、动态规划/判断子序列/src/com/company/Solution.java
+++ 算法与数据结构/算法练习/15、动态规划/判断子序列/src/com/company/Solution.java
@@ -9,19 +9,22 @@ package com.company;
 public class Solution {
 
     public static void main(String[] args) {
-        new Solution().isSubsequence("abc", "ahbgdc");
+        new Solution().isSubsequence("", "ahbgdc");
     }
 
     public boolean isSubsequence(String s, String t) {
         int m = s.length() + 1;
         int n = t.length() + 1;
         boolean[][] arr = new boolean[m][n];
+        for (int i = 0; i < n; i++) {
+            arr[0][i] = true;
+        }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    arr[i][j] = true;
+                    arr[i][j] = arr[i - 1][j - 1];
                 } else {
-                    arr[i][j] = arr[i - 1][j] || arr[i][j - 1];
+                    arr[i][j] = arr[i][j-1];
                 }
             }
         }
